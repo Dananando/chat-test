02aadbbef59bef301a5736cb0279d658bf61cfae
Dananando <dan.andriamihaja@gmail.com>
2021-07-16 15:06:49 +0200

    Users added but to be corrected

--- static/script/app-socket.js
+++ static/script/app-socket.js
@@ -6,13 +6,19 @@ const app = {
         const input = document.querySelector('#input');
         const messages = document.querySelector('#messages');
 
+        // Who are you?
+        const pseudo = app.whoAreYou();
+
+        // Alert that someone just got in
         socket.on('someone in', (msg) => {
             const someoneLi = document.createElement('li');
-            msg = 'Someone just connected';
+            msg = `${pseudo} jumped in the chat`;
+            someoneLi.style.fontStyle = 'italic';
             someoneLi.textContent = msg;  
             messages.appendChild(someoneLi);          
         });
 
+        // Poster un message sur le chat (envoi vers le back)
         form.addEventListener('submit', (event) => {
             event.preventDefault();
             if (input.value) {
@@ -21,19 +27,31 @@ const app = {
             }
         });
 
+        // Poster un message sur le chat - réception du message provenant du back
         socket.on('chat message', (msg) => {
             const item = document.createElement('li');
             item.textContent = msg;
             messages.appendChild(item);
             window.scrollTo(0, document.body.scrollHeight);
         });
-
+        
+        // Notification de déconnection
         socket.on('newDisconnect', (msg) => {
             const someoneOutLi = document.createElement('li');
-            msg = 'Someone just disconnected';
+            msg = `${pseudo} just disconnected`;
             someoneOutLi.textContent = msg;  
             messages.appendChild(someoneOutLi);
         });
+    },
+
+    // Choose you pseudo via a prompt()
+    whoAreYou() {
+        let pseudo;
+        while(!pseudo) {
+            pseudo = prompt('Choose a pseudo');
+        }
+
+        return pseudo;
     }
 };
 
